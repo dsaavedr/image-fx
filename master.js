28ff17e7ec32cddba81b94a7180fd8ade616c187
Daniel Saavedra <danielsaavedram@hotmail.com>
2022-05-27 14:20:34 -0500

    Added logic to draw image

--- master.js
+++ master.js
@@ -1,7 +1,13 @@
-let WIDTH, HEIGHT;
+let WIDTH, 
+    HEIGHT,
+    pixels = [];
+
+const imageURL = "https://i.redd.it/kxov0rcoi4p21.png",
+    SCALE = 0.5;
 
 const canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
+
 
 const requestAnimationFrame =
     window.requestAnimationFrame ||
@@ -9,18 +15,31 @@ const requestAnimationFrame =
     window.webkitRequestAnimationFrame ||
     window.msRequestAnimationFrame;
 
-function init() {
-    WIDTH = window.innerWidth;
-    HEIGHT = window.innerHeight;
+let img = new Image();
+img.src = imageURL;
+img.setAttribute("crossOrigin", "");
+img.onload = e => init(e);
 
-    canvas.setAttribute("width", WIDTH);
-    canvas.setAttribute("height", HEIGHT);
+function init(e) {
+    const { naturalHeight, naturalWidth } = e.path[0];
 
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    ctx.closePath();
+    WIDTH = naturalWidth;
+    HEIGHT = naturalHeight;
+
+    canvas.setAttribute("width", WIDTH * SCALE);
+    canvas.setAttribute("height", HEIGHT * SCALE);
+
+    ctx.drawImage(img, 0, 0, WIDTH * SCALE, HEIGHT * SCALE);
+
+    let imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+    let data = imgData.data;
+
+    log(imgData);
+
+    for (let i = 0; i < data.length; i += 4) {
+	pixels.push(new Pixel(data[i], data[i + 1], data[i + 2], data[i + 3]));
+    }
+    console.log(pixels);
 
     ani();
 }
