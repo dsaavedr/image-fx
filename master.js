5fad0ccad0104ee390a3edc4442cdb749d6392cf
Daniel Saavedra <danielsaavedram@hotmail.com>
2022-05-28 11:29:51 -0500

    Now creating image in greyscale

--- master.js
+++ master.js
@@ -3,7 +3,7 @@ let WIDTH,
     pixels = [];
 
 const imageURL = "https://i.redd.it/kxov0rcoi4p21.png",
-    SCALE = 0.5;
+    SCALE = 1;
 
 const canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
@@ -34,18 +34,36 @@ function init(e) {
     let imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
     let data = imgData.data;
 
-    log(imgData);
-
     for (let i = 0; i < data.length; i += 4) {
-	pixels.push(new Pixel(data[i], data[i + 1], data[i + 2], data[i + 3]));
+	// Add pixels in grey scale
+	const pixel = new Pixel(data[i], data[i + 1], data[i + 2], data[i + 3]); 
+	const luminance = pixel.luminance();
+	pixel.update({
+	    r: luminance,
+	    g: luminance,
+	    b: luminance
+	});
+	pixels.push(pixel);
     }
     console.log(pixels);
 
+    const newData = ctx.createImageData(imgData.width, imgData.height);
+
+    let pixelIdx = 0;
+    for (let i = 0; i < newData.data.length; i+= 4) {
+	const {r, g, b, a} = pixels[pixelIdx].getData();
+	newData.data[i] = r;
+	newData.data[i + 1] = g;
+	newData.data[i + 2] = b;
+	newData.data[i + 3] = a;
+	pixelIdx++;
+    }
+
+    ctx.putImageData(newData, 0, 0);
+    
     ani();
 }
 
 function ani() {
     //requestAnimationFrame(ani);
 }
-
-init();
